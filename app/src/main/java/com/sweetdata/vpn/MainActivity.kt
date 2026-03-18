c6621324ffbccfbec97decaee4fc16cb9ef58e85
cooperjane549-cmd <cooperjane549@gmail.com>
2026-03-18 13:18:29 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/sweetdata/vpn/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.sweetdata.vpn
+import android.content.Intent
+import android.net.VpnService
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sweetdata.vpn.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var vpnManager: WireGuardManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        vpnManager = WireGuardManager(this)
+        binding.btnConnect.setOnClickListener {
+            val intent = VpnService.prepare(this)
+            if (intent != null) {
+                startActivityForResult(intent, 0)
+            } else {
+                startVpn()
+            }
+        }
+    }
+    private fun startVpn() {
+        vpnManager.connect()
+    }
+}
