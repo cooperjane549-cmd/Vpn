c1a2f258cd7748163d7c6a5da05b1db7f1580199
cooperjane549-cmd <cooperjane549@gmail.com>
2026-03-18 21:42:13 +0300

    Update WireGuardManager.kt

--- app/src/main/java/com/sweetdata/vpn/WireGuardManager.kt
+++ app/src/main/java/com/sweetdata/vpn/WireGuardManager.kt
@@ -1,49 +1,61 @@
 package com.sweetdata.vpn
 
 import android.content.Context
-import com.wireguard.android.backend.GoBackend
+import android.util.Log
+import com.wireguard.android.backend.Tunnel
+import com.wireguard.android.backend.TunnelManager
+import com.wireguard.android.backend.TunnelManagerBackend
 import com.wireguard.config.Config
 import com.wireguard.config.Interface
 import com.wireguard.config.Peer
-import com.wireguard.android.backend.Tunnel
 
 class WireGuardManager(context: Context) {
 
-    private val backend = GoBackend(context)
+    private val tunnelManager: TunnelManagerBackend = TunnelManager.getBackend(context)
     private var tunnel: Tunnel? = null
 
     fun connect() {
+        try {
+            // Replace these with your server/client keys
+            val privateKey = "REPLACE_WITH_CLIENT_PRIVATE_KEY"
+            val publicKey = "REPLACE_WITH_SERVER_PUBLIC_KEY"
+            val endpoint = "YOUR_SERVER_IP:51820"
 
-        val privateKey = "REPLACE_WITH_CLIENT_PRIVATE_KEY"
-        val publicKey = "REPLACE_WITH_SERVER_PUBLIC_KEY"
+            val config = Config.Builder()
+                .setInterface(
+                    Interface.Builder()
+                        .addAddress("10.0.0.2/32")
+                        .setPrivateKey(privateKey)
+                        .build()
+                )
+                .addPeer(
+                    Peer.Builder()
+                        .setPublicKey(publicKey)
+                        .addAllowedIp("0.0.0.0/0")
+                        .setEndpoint(endpoint)
+                        .build()
+                )
+                .build()
 
-        val config = Config.Builder()
-            .setInterface(
-                Interface.Builder()
-                    .addAddress("10.0.0.2/32")
-                    .setPrivateKey(privateKey)
-                    .build()
-            )
-            .addPeer(
-                Peer.Builder()
-                    .setPublicKey(publicKey)
-                    .addAllowedIp("0.0.0.0/0")
-                    .setEndpoint("YOUR_SERVER_IP:51820")
-                    .build()
-            )
-            .build()
+            tunnel = object : Tunnel {
+                override fun getName() = "sweetdataTunnel"
+                override fun onStateChange(newState: Tunnel.State) {
+                    Log.d("WireGuardManager", "Tunnel state: $newState")
+                }
+            }
 
-        tunnel = object : Tunnel {
-            override fun getName() = "sweetdataTunnel"
-            override fun onStateChange(newState: Tunnel.State) {}
+            tunnelManager.setState(tunnel!!, Tunnel.State.UP, config)
+            Log.d("WireGuardManager", "VPN connected successfully!")
+
+        } catch (e: Exception) {
+            Log.e("WireGuardManager", "Failed to connect VPN", e)
         }
-
-        backend.setState(tunnel!!, Tunnel.State.UP, config)
     }
 
     fun disconnect() {
         tunnel?.let {
-            backend.setState(it, Tunnel.State.DOWN, null)
+            tunnelManager.setState(it, Tunnel.State.DOWN, null)
+            Log.d("WireGuardManager", "VPN disconnected")
         }
     }
 }
