d2906c68e51186f9fcc3ac9e2648adc60c6b46d6
cooperjane549-cmd <cooperjane549@gmail.com>
2026-03-19 09:27:26 +0300

    Update MainActivity.kt

--- app/src/main/java/com/sweetdata/vpn/MainActivity.kt
+++ app/src/main/java/com/sweetdata/vpn/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.sweetdata.vpn
 
+import android.app.Activity
 import android.content.Intent
 import android.net.VpnService
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sweetdata.vpn.databinding.ActivityMainBinding
-import com.wireguard.android.backend.WireGuardManager // Import the correct WireGuardManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var vpnManager: WireGuardManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        vpnManager = WireGuardManager(this)
 
         binding.btnConnect.setOnClickListener {
             val intent = VpnService.prepare(this)
@@ -30,6 +27,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startVpn() {
-        vpnManager.connect()
+        val intent = Intent(this, MyVpnService::class.java)
+        startService(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            startVpn()
+        }
     }
 }
